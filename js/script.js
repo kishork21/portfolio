ec6c765c78d6963f0cfde785438ab629288f2804
kishork21 <kishork212002@gmail.com>
2023-10-19 20:35:22 +0530

    created portfolio

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+function sidebar1() {
+    if (document.getElementById('links').style.display == 'none') {
+        document.getElementById('links').style.display = 'block';
+    } else {
+        document.getElementById('links').style.display = 'none';
+    }
+}
+
+function sidebar2() {
+    document.getElementById('links').style.display = 'block';
+}
+
+let portfolio = {
+    init: function () {
+        portfolio.textAutoType();
+        portfolio.bind();
+    },
+    bind: function () {
+
+        $('body').on('click', '#formSubmit', portfolio.sendEmail)
+    },
+    textAutoType: function () {
+        var typed = new Typed('.auto-type', {
+            strings: ['Web Developer'],
+            typeSpeed: 200,
+            backSpeed: 50,
+            loop: true,
+        })
+    },
+    sendEmail: function () {
+        Email.send({
+            Host: 'smtp.elasticemail.com',
+            Username: 'mewadakunal2002@gmail.com',
+            Password: '1A8BAE52111655EE76B88A8FE2F35238F80D',
+            To: 'kishork212002@gmail.com',
+            From: 'mewadakunal2002@gmail.com',
+            Subject: 'From unknown user',
+            Body: {
+                name: document.getElementById('name').value,
+                number: document.getElementById('number').value,
+                email: document.getElementById('email').value,
+                msg: document.getElementById('msg').value
+            }
+        }).then(
+            message => {
+                if (message === 'OK') {
+                    alert('Details sent successfully');
+                    $('body #contact-form input,#contact-form textarea').val('')
+                }
+            }
+        )
+    }
+}
+portfolio.init()
